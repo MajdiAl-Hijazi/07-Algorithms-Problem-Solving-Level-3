3e30252e9d17897a11ff3c300a40e9a5b9aeaea0
Majdi AlHijazi <majdialhijazi@gmail.com>
2026-01-26 18:02:27 +0300

    Add files via upload

---
+++ #35_PrintEachWordInString.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+#include<string>
+
+using namespace std;
+
+string ReadString() {
+
+    string S1;
+    cout << "Enter your string ?\n";
+    getline(cin, S1);
+    return S1;
+}
+
+void PrintEachWordinString(string S1) {
+
+    cout << "\nYour string words  are :" << endl;
+
+    string delim = " ";
+    string Word;
+    short pos = 0;
+
+    while ((pos = S1.find(delim)) != string::npos) {
+    
+        Word = S1.substr(0 ,pos);
+
+        if (Word != "")
+            cout << Word << endl;
+
+		S1.erase(0, pos + delim.length());
+    }
+
+    if (S1 != "")
+        cout << S1 << endl;
+
+}
+
+int main() {
+
+    PrintEachWordinString(ReadString());
+
+    return 0;
+}
