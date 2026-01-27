b52e6c2e1f702b0928cccde6612061cea6f3450e
Majdi AlHijazi <majdialhijazi@gmail.com>
2026-01-27 16:08:05 +0300

    Add files via upload

---
+++ #38_TrimLeft-TrimRight-Trim.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+string ReadString() {
+
+    string S1;
+    cout << "Enter your string ?\n";
+    getline(cin, S1);
+    return S1;
+}
+
+string TrimLeft(string S1) {
+
+    for (short i = 0; i < S1.length(); i++) {
+    
+        if (S1[i] != ' ')
+            return S1.substr(i, S1.length() - i);
+    }
+
+    return "";
+}
+
+string TrimRight(string S1) {
+
+    for (short i = S1.length() - 1; i >= 0; i--) {
+    
+        if (S1[i] != ' ')
+            return S1.substr(0, i + 1);
+    }
+
+    return "";
+}
+
+string Trim(string S1) {
+
+    return TrimRight(TrimLeft(S1));
+}
+
+int main() {
+
+    string S1 = ReadString();
+    cout << "\nString =" << S1 << endl;
+    cout << "\nTrimLeft =" << TrimLeft(S1) << endl;
+    cout << "\nTrimRight =" << TrimRight(S1) << endl;
+    cout << "\nTrim =" << Trim(S1) << endl;
+
+	return 0;
+}
