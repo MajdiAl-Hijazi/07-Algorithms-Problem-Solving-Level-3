d0d502ea0a2e880ab1ea90d308b9015c3fcbde13
Majdi AlHijazi <majdialhijazi@gmail.com>
2026-01-28 15:16:13 +0300

    Add files via upload

---
+++ #42_ReplaceWords.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+string ReadString() {
+
+    string S1;
+    cout << "Enter your string ?\n";
+    getline(cin, S1);
+    return S1;
+}
+
+string ReplaceWordInStringUsingBuiltInFunction(string S1, string StringToReplace, string sReplaceTo) {
+
+    short pos = S1.find(StringToReplace);
+
+    while (pos != string::npos) {
+    
+        S1.replace(pos ,StringToReplace.length(), sReplaceTo);
+        pos = S1.find(StringToReplace);
+    }
+
+    return S1;
+}
+
+int main() {
+
+    string S1 = "Welcome to Jordan , Jordan is a nice country";
+    cout << "\nString brfore replace :\n" << S1 << endl;
+    cout << "\nString after replace :\n" << ReplaceWordInStringUsingBuiltInFunction(S1, "Jordan", "USA") << endl;
+
+    return 0;
+}
