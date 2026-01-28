2c835836181a229ebee113e53eb2bdbc18daa988
Majdi AlHijazi <majdialhijazi@gmail.com>
2026-01-28 13:13:00 +0300

    Add files via upload

---
+++ #40+JoinString(OverLoading).cpp
@@ -0,0 +1,46 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+string JoinString(const vector<string>& vStrings, string Delim) {
+
+	if (vStrings.size() == 0)
+		return "";
+
+	string S1 = "";
+
+	for (const string& Token : vStrings) {
+
+		S1.append(Token);
+		S1.append(Delim);
+	}
+
+	return S1.substr(0, S1.length() - Delim.length());
+}
+
+string JoinString(string arr[], short Length, string Delim) {
+
+	if (Length == 0)
+		return "";
+
+	string S1 = "";
+
+	for (short i = 0; i < Length; i++) {
+	
+		S1.append(arr[i]);
+		S1.append(Delim);
+	}
+
+	return S1.substr(0, S1.length() - Delim.length());
+}
+
+int main() {
+
+	vector<string> vStrings{ "majdi", "aws", "owen", "shahem" };
+	cout << "Vector after join:\n" << JoinString(vStrings ," ") << endl;
+	string arr[]{ "majdi", "aws", "owen", "shahem" };
+	cout << "\n\nArray after join:\n" << JoinString(arr, 4, " ") << endl;
+
+	return 0;
+}
