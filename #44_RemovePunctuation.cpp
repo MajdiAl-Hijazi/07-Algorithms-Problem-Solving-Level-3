2fb0e0acd577d628c0c59f4e2f1847a89982d278
Majdi AlHijazi <majdialhijazi@gmail.com>
2026-01-28 19:45:47 +0300

    Add files via upload

---
+++ #44_RemovePunctuation.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<string>
+#include<cctype>
+using namespace std;
+
+string ReadString() {
+
+    string S1;
+    cout << "Enter your string ?\n";
+    getline(cin, S1);
+    return S1;
+}
+
+string RemoveAllPunctuation(string S1) {
+
+    string S2 = "";
+
+    for (short i = 0; i < S1.length(); i++) {
+    
+        if (!ispunct(S1[i]))
+            S2 += S1[i];
+    }
+
+    return S2;
+}
+
+int main() {
+
+    string S1 = "Welcome to Jordan, Jordan is a nice country; it's amazing.";
+    cout << "Orginal string :\n" << S1 << endl;
+    cout << "\nPunctuation Removed :\n" << RemoveAllPunctuation(S1) << endl;
+
+    return 0;
+}
