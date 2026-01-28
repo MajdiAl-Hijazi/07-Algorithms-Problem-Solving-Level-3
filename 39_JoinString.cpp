a75c2289854866ea20cd01e900e64daa11d8d8f4
Majdi AlHijazi <majdialhijazi@gmail.com>
2026-01-28 13:12:22 +0300

    Add files via upload

---
+++ 39_JoinString.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+string JoinString(const vector<string>& vStrings, string Delim) {
+
+	if (vStrings.empty())
+		return "";
+
+	string S1 = "";
+
+	for (const string& Token: vStrings) {
+	
+		S1.append(Token);
+		S1.append(Delim);
+	}
+
+	return S1.substr(0, S1.length() - Delim.length());
+}
+
+int main() {
+
+	vector<string> vStrings{ "majdi", "aws", "owen", "shahem"};
+	cout << "\nS1 = " << JoinString(vStrings ," ") << endl;
+
+	return 0;
+}
