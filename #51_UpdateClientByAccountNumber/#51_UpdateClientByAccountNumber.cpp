68d9ae015d68ca40294b27f996904b4d8550e209
Majdi AlHijazi <majdialhijazi@gmail.com>
2026-01-30 17:43:28 +0300

    Update #51_UpdateClientByAccountNumber.cpp

--- #51_UpdateClientByAccountNumber/#51_UpdateClientByAccountNumber.cpp
+++ #51_UpdateClientByAccountNumber/#51_UpdateClientByAccountNumber.cpp
@@ -1,1 +1,16 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+const string ClientsFileName = "Clients.txt";
+
+struct sClient {
+
+	string AccountNumber;
+	string PinCode;
+	string Name;
+	string Phone;
+	double AccountBalance;
+	bool MarkForDelete = false;
